72726822401f5562d14e18b26108f0c47cbd58c4
Patrik Olsson <patrik.olsson@arbetsformedlingen.se>
2019-04-11 18:00:24 +0200

    Added Material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatFormFieldModule, MatToolbarModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AdsComponent } from './ads/ads.component';
@@ -11,7 +13,8 @@ import { AdsComponent } from './ads/ads.component';
     AdsComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatButtonModule, MatInputModule, MatFormFieldModule,
+    MatToolbarModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
