b30d511da931032123fc2e3dbcc257b5266837e2
Erik Melander <56152878+erikmelanderAF@users.noreply.github.com>
2020-02-06 13:35:46 +0100

    Update ad.service.ts
    Changed to new production url.

--- src/app/ad.service.ts
+++ src/app/ad.service.ts
@@ -13,7 +13,7 @@ export class AdService {
 
   constructor(private http: HttpClient) {
     this.jsEnvironements = [
-      new JSEnvironment("Production", "https://open-api.dev.services.jtech.se", "cGF0cmlrLm9sc3NvbkBhcmJldHNmb3JtZWRsaW5nZW4uc2U"),
+      new JSEnvironment("Production", "https://jobsearch.api.jobtechdev.se/", "cGF0cmlrLm9sc3NvbkBhcmJldHNmb3JtZWRsaW5nZW4uc2U"),
       new JSEnvironment("Staging", "https://staging-jobs.dev.services.jtech.se", "cGF0cmlrLm9sc3NvbkBhcmJldHNmb3JtZWRsaW5nZW4uc2U"),
       new JSEnvironment("Dev", "https://dev-jobsearch-api.jobtechdev.se/", "cGF0cmlrLm9sc3NvbkBhcmJldHNmb3JtZWRsaW5nZW4uc2U"),
       new JSEnvironment("i1", "https://i1-open-api.dev.services.jtech.se", "cGF0cmlrLm9sc3NvbkBhcmJldHNmb3JtZWRsaW5nZW4uc2U")
